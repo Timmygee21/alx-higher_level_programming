d2f5fc9638251d7ebcf799bfbe2eb1d0f3b0bb54
Timmygee21 <saytimmy@gmail.com>
2023-01-11 13:16:24 +0100

    A function that returns a set of all elements present in only one set

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def unique_elements(set1, set2):
+    unique_elements = set1.symmetric_difference(set2)
+    return unique_elements
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def count_keys(dictionary):
+    return len(dictionary)
