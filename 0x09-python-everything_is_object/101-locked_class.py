f6dc2bf5a3b62637b1f53ebced0a98efb524be70
Timmygee21 <saytimmy@gmail.com>
2023-02-01 08:00:49 +0100

    class LockedClass with no class or object attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,18 @@
-#!/usr/bin/python3 
- """Defines a locked class.""" 
-  
-  
- class LockedClass: 
-     """ 
-     Prevent the user from instantiating new LockedClass attributes 
-     for anything but attributes called 'first_name'. 
-     """ 
-  
-     __slots__ = ["first_name"]
+#!/usr/bin/python3
+# 101-locked_class.py
+"""
+This is a module that containts a clas that avoids
+dynmaically created attributes
+"""
+
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Init method """
+        pass
