35b657358601e8e48a077ea32d561be0aacb11ba
Timmygee21 <saytimmy@gmail.com>
2023-02-08 06:02:19 +0100

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3ads from standard input and computes metrics.
-
-"""
+#!/usr/bin/python3
+# 101-stats.py
+"""Reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
@@ -17,6 +17,7 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
@@ -55,3 +56,8 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Pri
